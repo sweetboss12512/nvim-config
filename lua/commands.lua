d30631ca11873f4bbecc75cd1f9aade97c6089b9
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-11-02 18:08:16 -1100

    Command to close all other buffers

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,3 +1,14 @@
 vim.api.nvim_create_user_command("Config", function()
 	vim.cmd(":tabedit $MYVIMRC/..")
 end, {})
+
+vim.api.nvim_create_user_command("BdOthers", function()
+	local current_bufnr = vim.api.nvim_get_current_buf()
+	local bufnr_list = vim.api.nvim_list_bufs()
+
+	for _, bufnr in ipairs(bufnr_list) do
+		if bufnr ~= current_bufnr then
+			vim.api.nvim_buf_delete(bufnr, {})
+		end
+	end
+end, {})
