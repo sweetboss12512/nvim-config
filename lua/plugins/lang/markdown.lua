ebc95147838f0511a8e34f3cd171f65bcac13f07
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-12-06 17:18:29 -0500

    fail

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -8,6 +8,14 @@ local renderMarkdown = {
 	},
 }
 
+-- local function extend_hl(name, def)
+-- 	local current_def = vim.api.nvim_get_hl(0, { name = name })
+-- 	local new_def = vim.tbl_extend("force", {}, current_def, def)
+--
+-- 	vim.api.nvim_set_hl(0, name, new_def)
+-- 	return name
+-- end
+
 local markview = {
 	"OXY2DEV/markview.nvim",
 	lazy = false, -- Recommended
@@ -18,10 +26,31 @@ local markview = {
 		"nvim-tree/nvim-web-devicons",
 	},
 	opts = {
-		filetypes = { "markdown", "quarto", "rmd", "help" },
+		filetypes = { "markdown", "quarto", "rmd" },
+		headings = {
+			enable = true,
+
+			--- Amount of character to shift per heading level
+			---@type integer
+			shift_width = 1,
+
+			heading_1 = {
+				style = "icon",
+
+				shift_hl = "DiffAdd",
+
+				--- Background highlight group.
+				---@type string
+				hl = "@markup.heading.1.markdown",
+			},
+			heading_2 = {
+				hl = "@markup.heading.2.markdown",
+			},
+		},
 	},
 }
 
+-- markdownH1     xxx cterm=bold gui=bold guifg=#c4a7e7
 return {
 	renderMarkdown,
 	-- markview,
