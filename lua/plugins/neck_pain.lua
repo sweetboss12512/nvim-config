8b7274346f3d74735cf02bdafe828a13011067ea
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-12-08 11:00:59 -0500

    this is annoying

--- lua/plugins/neck_pain.lua
+++ lua/plugins/neck_pain.lua
@@ -7,8 +7,14 @@ return {
 			"<cmd>NoNeckPain<cr>",
 			desc = "No Neck Pain (Toggle)",
 		},
+		{
+			"<leader>nl",
+			"<cmd>NoNeckPainToggleRightSide<cr>",
+			desc = "No Neck Pain (Toggle)",
+		},
 	},
 	opts = {
 		-- width = 120,
+		width = 100,
 	},
 }
