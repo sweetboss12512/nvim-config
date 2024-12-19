2e65fd05153eb6c6ea43ebf9cbdd07ab79ef084c
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2024-12-18 21:10:29 -0500

    switch from harpoon to grapple (better)

---
+++ lua/plugins/grapple.lua
@@ -0,0 +1,23 @@
+return {
+	"cbochs/grapple.nvim",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons", lazy = true },
+	},
+	opts = {
+		scope = "git_branch",
+		win_opts = {
+			border = "single",
+			-- footer = "",
+		},
+	},
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = "Grapple",
+	keys = {
+		{ "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
+		{ "<leader>e", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
+		{ "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+	},
+}
