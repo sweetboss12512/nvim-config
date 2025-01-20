19f9bfb008ab2234c10947e3a44ffffa9e1e8e83
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-01-20 09:02:28 -0500

    comment.nvim change eol, update plugins

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -8,7 +8,7 @@ return {
 			ignore = nil,
 			toggler = { line = "gcc", block = "gbc" },
 			opleader = { line = "gc", block = "gb" },
-			extra = { above = "gcO", below = "gco", eol = "gcA" },
+			extra = { above = "gcO", below = "gco", eol = "gA" },
 			mappings = { basic = true, extra = true },
 			pre_hook = nil,
 			post_hook = nil,
