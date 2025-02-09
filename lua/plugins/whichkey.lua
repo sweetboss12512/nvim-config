803c5b26bba60e85f4d742a0fedbfc2011aae9f2
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-02-09 11:17:40 -0500

    whichkey preset helix

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,8 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+        preset = "helix",
+        -- preset = "modern",
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
