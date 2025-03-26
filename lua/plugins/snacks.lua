f277abb0308dfc3353ac9e533632595fd46f48dc
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-03-26 19:49:18 -0400

    Remove snacks.rename oil integration
    Oil already does LSP rename already. Pyright just sucks and doesn't
    support it :/

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -187,15 +187,6 @@ return {
                 end
                 vim.notify(table.concat(print_safe_args, " "))
             end
-
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "OilActionsPost",
-                callback = function(event)
-                    if event.data.actions.type == "move" then
-                        Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-                    end
-                end,
-            })
         end,
     },
 
