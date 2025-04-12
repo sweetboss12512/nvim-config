5afe10502e3d94fe4b15c55c069b81b043467704
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-04-12 13:16:23 -0400

    Cleanup nvim-lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,18 +1,61 @@
+local function debounce(ms, callback)
+    local timer = vim.uv.new_timer()
+    return function(...)
+        local args = { ... }
+        timer:stop()
+        timer:start(ms, 0, function()
+            vim.schedule_wrap(callback)(unpack(args))
+        end)
+    end
+end
+
 return {
     "mfussenegger/nvim-lint",
-    config = function()
-        require("lint").linters_by_ft = {
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        linters_by_ft = {
             lua = { "selene" },
             luau = { "selene" },
             html = { "djlint" },
             jinja = { "djlint" },
             htmldjango = { "djlint" },
-        }
+        },
+        linters = {
+            selene = {
+                -- condition = function(_, bufnr)
+                --     return vim.fs.root(bufnr, "selene.toml")
+                -- end,
+            },
+        },
+    },
+
+    config = function(_, opts)
+        local lint = require("lint")
+        lint.linters_by_ft = opts.linters_by_ft
+
+        for name, linter in pairs(opts.linters) do
+            if type(linter) == "table" and type(lint.linters[name]) == "table" then
+                lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+            else
+                lint.linters[name] = linter
+            end
+        end
+
+        local function try_lint()
+            local names = vim.tbl_filter(function(name)
+                local linter = lint.linters[name]
+                if type(linter) == "table" and linter.condition then
+                    return linter:condition(vim.api.nvim_get_current_buf())
+                end
+                return true
+            end, lint._resolve_linter_by_ft(vim.bo.filetype))
+            if #names > 0 then
+                lint.try_lint(names)
+            end
+        end
 
         vim.api.nvim_create_autocmd({ "BufReadPost", "InsertLeave", "TextChanged" }, {
-            callback = function()
-                require("lint").try_lint()
-            end,
+            callback = debounce(100, try_lint),
         })
     end,
 }
