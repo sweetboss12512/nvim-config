7887dc7c725d4ee006443ccbee91c94d65082d52
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-04-12 13:16:41 -0400

    Tried nvim-surround with 'helix' keybinds

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,4 +1,35 @@
 return {
     "kylechui/nvim-surround",
-    opts = {},
+    event = "VeryLazy",
+    opts = {
+        keymaps = {
+
+            -- #region Defaults
+            insert = "<C-g>s",
+            insert_line = "<C-g>S",
+            normal = "ys",
+            normal_cur = "yss",
+            normal_line = "yS",
+            normal_cur_line = "ySS",
+            visual = "S",
+            visual_line = "gS",
+            delete = "ds",
+            change = "cs",
+            change_line = "cS",
+            --#endregion
+
+            -- helix-like: Mainly wanted to map multicursor split to 'v_S'
+            -- insert = "<C-g>s",
+            -- insert_line = "<C-g>S",
+            -- normal = "ms",
+            -- normal_cur = "mss",
+            -- normal_line = "mS",
+            -- normal_cur_line = "mSS",
+            -- visual = "ms",
+            -- visual_line = "gS",
+            -- delete = "md",
+            -- change = "mr",
+            -- change_line = "mS",
+        },
+    },
 }
