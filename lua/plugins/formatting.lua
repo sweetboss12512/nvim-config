b94baaf54f36dc0eb7bc2978d84456f3b5bde587
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-04-12 13:15:04 -0400

    Cleanup conform.nvim

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,45 +1,40 @@
-local auto_format = true
-
 return {
     "stevearc/conform.nvim",
-    lazy = false,
-    config = function()
-        local conform = require("conform")
-        conform.setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                luau = { "stylua" },
-                nix = { "alejandra" },
-                python = { "black" },
-                html = { "djlint" },
-                jinja = { "djlint" },
-                htmldjango = { "djlint" },
-            },
-            {
-                format_on_save = {
-                    -- These options will be passed to conform.format()
-                    timeout_ms = 500,
-                    lsp_fallback = true,
-                },
-            },
-        })
-
-        vim.api.nvim_create_autocmd("BufWritePre", { -- Auto format
-            pattern = "*",
-            callback = function(args)
-                if auto_format then
-                    require("conform").format({ bufnr = args.buf })
-                end
+    event = "BufWritePre",
+    keys = {
+        {
+            "<F3>",
+            function()
+                require("conform").format()
             end,
-        })
+            desc = "Foramt Buffer (Conform)",
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            luau = { "stylua" },
+            nix = { "alejandra" },
+            python = { "black" },
+            html = { "djlint" },
+            jinja = { "djlint" },
+            htmldjango = { "djlint" },
+        },
 
+        format_on_save = function()
+            if vim.g.autoformat then
+                return {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                }
+            end
+        end,
+    },
+    init = function()
+        vim.g.autoformat = true
         vim.api.nvim_create_user_command("ToggleAutoFormat", function()
-            auto_format = not auto_format
-            vim.notify("Auto format set to " .. tostring(auto_format))
+            vim.g.autoformat = not vim.g.autoformat
+            vim.notify("Auto format set to " .. tostring(vim.g.autoformat))
         end, {})
-
-        vim.keymap.set("n", "<F3>", function()
-            require("conform").format({ async = true, lsp_fallback = true })
-        end)
     end,
 }
