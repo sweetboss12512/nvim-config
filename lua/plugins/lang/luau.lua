e75941c594cb1a4794e7fecc818d91801d74c575
sweetboss12512 <101525599+sweetboss12512@users.noreply.github.com>
2025-05-03 13:09:58 -0400

    Remove taking fileAliases from vscode settings
    .luaurc is where aliases should be now.

--- lua/plugins/lang/luau.lua
+++ lua/plugins/lang/luau.lua
@@ -1,17 +1,3 @@
-local util = require("util")
-
-local directoryAliases
-local fileAliases
-
-local function update_aliases()
-    local vscodeSettings = util.vscode_settings()
-
-    if vscodeSettings then
-        directoryAliases = vscodeSettings["luau-lsp.require.directoryAliases"]
-        fileAliases = vscodeSettings["luau-lsp.require.fileAliases"]
-    end
-end
-
 local function rojo_project()
     return vim.fs.root(0, function(name)
         return name:match(".+%.project%.json$")
@@ -23,67 +9,61 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     ft = { "luau" },
     cmd = { "LuauLsp" },
-    config = function()
-        local luau_lsp = require("luau-lsp")
-        update_aliases()
-        luau_lsp.setup({
-            platform = {
-                type = rojo_project() and "roblox" or "standard",
-            },
-            autostart = true,
-            server = {
-                settings = {
-                    ["luau-lsp"] = {
-                        require = {
-                            directoryAliases = directoryAliases,
-                            fileAliases = fileAliases,
-                            inlayHints = {
-                                functionReturnTypes = true,
-                                parameterTypes = true,
-                            },
+    opts = {
+        platform = {
+            type = rojo_project() and "roblox" or "standard",
+        },
+        autostart = true,
+        server = {
+            settings = {
+                ["luau-lsp"] = {
+                    require = {
+                        inlayHints = {
+                            functionReturnTypes = true,
+                            parameterTypes = true,
                         },
-                        ignoreGlobs = {
-                            -- Wally
-                            "**/_Index/**",
-                            ".nvim.lua",
+                    },
+                    ignoreGlobs = {
+                        -- Wally
+                        "**/_Index/**",
+                        ".nvim.lua",
 
-                            -- Pesde Stuff
-                            ".pesde/**",
-                            "*_packages/**",
-                        },
-                        completion = {
-                            -- autocompleteEnd = true,
-                            fillCallArguments = false,
-                            addParentheses = false,
-                            imports = {
-                                enabled = true,
-                                suggestServices = true,
-                                suggestRequires = true,
-                                requireStyle = "alwaysAbsolute",
-                                ignoreGlobs = {
-                                    -- Wally
-                                    "**/_Index/**",
+                        -- Pesde Stuff
+                        ".pesde/**",
+                        "*_packages/**",
+                    },
+                    completion = {
+                        -- autocompleteEnd = true,
+                        fillCallArguments = false,
+                        addParentheses = false,
+                        imports = {
+                            enabled = true,
+                            suggestServices = true,
+                            suggestRequires = true,
+                            requireStyle = "alwaysAbsolute",
+                            ignoreGlobs = {
+                                -- Wally
+                                "**/_Index/**",
 
-                                    -- Pesde Stuff
-                                    ".pesde/**",
-                                    "*_packages/.pesde/**",
-                                },
+                                -- Pesde Stuff
+                                ".pesde/**",
+                                "*_packages/.pesde/**",
                             },
                         },
                     },
                 },
             },
-            plugin = {
-                enabled = rojo_project() ~= nil,
-                port = 3667,
-            },
-            sourcemap = {
-                enabled = false,
-                autogenerate = false,
-            },
-            types = {
-                -- roblox_security_level = "PluginSecurity",
-            },
-        })
-    end,
+        },
+        plugin = {
+            enabled = rojo_project() ~= nil,
+            port = 3667,
+        },
+        sourcemap = {
+            enabled = false,
+            autogenerate = false,
+        },
+        types = {
+            -- roblox_security_level = "PluginSecurity",
+        },
+    },
 }
